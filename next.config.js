5151ad88042a6356076f1f6e0585c85fb543811b
Your NameChris <chris@uktranscription.com>
2025-03-10 16:52:52 +0000

    Add public directory and next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: ['mhzptpexihkvyazmecoc.supabase.co'],
+  },
+}
+
+module.exports = nextConfig 
